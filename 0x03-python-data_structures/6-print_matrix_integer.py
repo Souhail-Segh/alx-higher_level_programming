ff5c124dd242132cea204d9924dfdd90c97caaa7
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-14 10:15:40 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for r in matrix:
+        if r:
+            for idx in range(len(r) - 1):
+                print('{:d} '.format(r[idx]), end='')
+            print('{:d} '.format(r[-1]))
+           
+            
