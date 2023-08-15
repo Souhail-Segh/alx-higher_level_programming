7e47ebc6bb4a4708f0b39e2302c1cf7fb917119f
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-15 14:06:46 -0700

    test correction 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list != None:
+    if my_list:
         if idx >= 0 and idx < len(my_list):
             return (my_list[idx])
     return (None)
-            
