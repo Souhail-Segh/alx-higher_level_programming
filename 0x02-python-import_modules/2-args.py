1d62d38a9b189bbd17a6fb2439f8014496e2345f
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-15 15:10:39 -0700

    sys module for command line args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
-
-args = sys.argv
-if len(args) == 1:
-    print("0 arguments.")
-else:
-    print("{:d} arguments:".format(len(args) - 1))
-    for i  in range(1,len(args)):
-        print("{:d}: {:s}".format(i, args[i]))
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(len(args) - 1))
+        for i  in range(1,len(args)):
+            print("{:d}: {:s}".format(i, args[i]))
