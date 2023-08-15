0756f110ddabb0ea956a239b3c677686e631349c
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-15 16:32:40 -0700

    100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    if args[2] not in ['+', '-', '*', '/']:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    a = int(args[1])
+    b = int(args[3])
+    op = args[2]
+    if op == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    if op == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    if op == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    if op == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    exit(0)
