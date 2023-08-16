6b1c7279c5654d7e8f95acf69220a2da89412212
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-16 13:13:53 -0700

    Task 4 - print different values of two sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    common = [x for x in set_1 if x not in set_2]
-    return common
+def only_diff_elements(set_1, set_2):
+    diff = [x for x in set_1 if x not in set_2]
+    return diff
