fa7908269251b3cf52684ed964ada4b2df0f4d3b
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-18 14:11:12 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string:
-        if type(roman_string) != type(' '):
+        if isinstance(roman_string, str) == False:
             return 0
         sum = 0
         prev = ''
-        roman = {'I': 1, 'V': 5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for s in roman_string:
             if s in roman:
                 sum += roman[s]
                 if (s in ['V', 'X']) and (prev == 'I'):
-                    sum -= 1
+                    sum -= 2
                 elif (s in ['L', 'C'] and (prev == 'X')):
-                    sum -= 10
-                elif (s in ['D', 'M']) and prev == 'C':
-                    sum -= 100
+                    sum -= 20
+                elif (s in ['D', 'M']) and (prev == 'C'):
+                    sum -= 200
                 prev = s
             else:
                 return 0
