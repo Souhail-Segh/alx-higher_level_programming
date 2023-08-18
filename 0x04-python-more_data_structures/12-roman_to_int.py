1f519cb62a2fd65ab9dc7d000f7c922f772760c4
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-18 14:13:02 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string:
-        if isinstance(roman_string, str) == False:
+        if isinstance(roman_string, str) is False:
             return 0
         sum = 0
         prev = ''
