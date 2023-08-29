4c99466445499c29d38321c89dfd377dffbdb3a6
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-29 13:42:44 -0700

    Task0 in classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """ Class of square
+    Attributes:
+        __init__(): initialise the Class
+
+    Todo:
+        Get an empty dict
+    """
+    def __init__(self):
+        """Function  that  give  square
+
+        Args:
+            __dict__: empty dictionary
+
+        Return:
+            None
+
+        """
+        self.__dict__ = {}
