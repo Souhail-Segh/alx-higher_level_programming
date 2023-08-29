ad6491688da33b563ae5334579500d8f315c02b8
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-08-29 14:27:20 -0700

    Task1: private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define square class. """
+
+
+class Square:
+    """ Square Reprentation. """
+
+    def __init__(self, size):
+        """Initialize a square.
+
+        Args:
+            size (int): size of new square
+        """
+
+        self.__size = size
