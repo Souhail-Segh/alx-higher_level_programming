b4da146332c0d958a3d162a85e3182b38cb77f2e
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-07 12:53:02 -0700

    Task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,19 @@
 
 
 def say_my_name(first_name, last_name=""):
+    ''' Function that concatenate two strings. '''
+    '''
+    print(first_name + last_name)
+    print(type(first_name))
+    print(not isinstance(first_name, str))
+    print(not isinstance(last_name, str))
+    '''
 
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
-
-    return first_name + last_name
+    space = ' '
+    if last_name == '':
+        space = ''
+    print('My name is {}{}{}'.format(first_name, space, last_name))
