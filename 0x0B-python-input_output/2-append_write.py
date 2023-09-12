05ab7ebcaf291e85531ca0244b4f9a8c36572c59
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-12 15:20:30 -0700

    Append file (return)

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Defines a function that read text from a file."""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Print the contents of a text file with UTF8 encoding."""
     with open(filename, "a", encoding="utf-8") as f:
         return(f.write(text))
