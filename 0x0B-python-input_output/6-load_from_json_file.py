e72cb0b4ed0ea7d4d9bcbad24177a015c00a4ba7
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-13 19:56:36 -0700

    Task 6: load from json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """Read an object to a text file using json representation."""
     with open(filename, "r") as f:
-        json.load(f)
+        return(json.load(f))
