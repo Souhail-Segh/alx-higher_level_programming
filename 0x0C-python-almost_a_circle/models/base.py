68f89cf844c131a393e12b2dff8cc50ca79fe556
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-18 03:15:23 -0700

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Declaration of a Base Class '''
+
+
+class Base:
+    ''' A base Class.
+
+    Initialisation of class variables:
+        __nb_objects: private
+        id
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        ''' Constructor methos '''
+        self.__nb_objects = 0
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
