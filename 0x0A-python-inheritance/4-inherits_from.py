b364440866bade028b69f8bfe6487a9f689a8075
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-27 07:54:37 -0700

    Task 4/   1

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ Function that check is one obj is instance of another obj
 def inherits_from(obj, a_class):
     ''' isinstance function '''
 
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
