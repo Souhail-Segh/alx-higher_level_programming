6b282288cd224893b8d509e89658e8122c1e64eb
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-09-27 07:36:24 -0700

    Tak 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ Function that check is one obj is instance of another obj
 def is_kind_of_class(obj, a_class):
     ''' isinstance function '''
 
-    if (issubclass(obj, a_class) and type(obj) != a_class):
+    if (issubclass(obj, a_class) and type(obj) != a_class:
         return True
     else:
         return False
