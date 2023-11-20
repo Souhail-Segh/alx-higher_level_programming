b1236ff4aff48b4a0dba1ba5d6123259b17ff92a
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-11-19 22:23:38 -0800

    Task 0.2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,11 +5,10 @@ import sys
 
 if __name__ == '__main__':
     ''' The follow code will not be exuceted when imported '''
-    print(sys.argv[1])
     conn = MySQLdb.connect(host="localhost",
-                                   port=3306, user= sys.argv[1],
-                                   passwd= sys.argv[2], db=sys.argv[3],
-                                   charset="utf8")
+                           port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3],
+                           charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
