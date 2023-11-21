726c5c1299f64d2726b1973b7e373571471b94b9
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-11-21 14:53:47 -0800

    task4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                            charset="utf8")
     cur = conn.cursor()
     cur.execute("""
-    SELECT cities.id, cities.name, state.name  FROM cities
+    SELECT cities.id, cities.name, states.name  FROM cities
     INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC
     """)
     query_rows = cur.fetchall()
