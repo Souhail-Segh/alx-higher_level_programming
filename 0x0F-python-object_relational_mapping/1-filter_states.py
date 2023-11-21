b8c4ace7f172d5f5a62baa431c418f82175959a1
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-11-21 13:49:26 -0800

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" lists all states from hbtn_0e_0_usa database when name start with N"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    ''' The follow code will not be exuceted when imported '''
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
