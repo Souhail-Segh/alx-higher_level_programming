740895e733c8d6de5a693a2c580c2c8c6080e4ff
Souhail-Segh <souhail.segh.ds@gmail.com>
2023-11-21 15:06:34 -0800

    task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cur.execute("""
     SELECT cities.name FROM cities
     INNER JOIN states ON cities.state_id = states.id
-    WHERE states.name = % ORDER BY cities.id ASC
+    WHERE states.name = %s ORDER BY cities.id ASC
     """, (input,))
     query_rows = cur.fetchall()
     for row in query_rows:
