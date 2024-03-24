ca6341f01ce384cf7321aef805df50c5b981d326
Souhail-Segh <souhail.segh.ds@gmail.com>
2024-03-24 05:30:04 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,22 +3,16 @@
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    ''' The follow code will not be exuceted when imported '''
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3],
-                           charset="utf8")
-    cur = conn.cursor()
-    input = sys.argv[4]
 
-    cur.execute("""
-    SELECT cities.name FROM cities
-    INNER JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s ORDER BY cities.id ASC
-    """, (input,))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
     cur.close()
-    conn.close()
+    db.close()
